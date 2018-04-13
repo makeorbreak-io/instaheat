21238217b713e53514f7287a5d7ca9d6a7d3f7b5
João Domingues <joaopldomingues@gmail.com>
2018-04-13 18:05:47 +0100

    changes in ard file

--- mob_projeto/mob_projeto.ino
+++ mob_projeto/mob_projeto.ino
@@ -1,21 +1,50 @@
 #include <Wire.h>    /* Communication with Arduino */
 #include <rgb_lcd.h> /* LCD */
-#include <WiFi.h>    /* WiFi? */
+#include <WiFi.h>
 
-/**
-   Declaring the 16x2 lcd
-*/
+// PIN's
+//input
+int tempPin = 1;            // temperature sensor
+int moreTempButtonPin = 1;  // button to increase temperature
+int lessTempButtonPin = 1;  // button to decrease temperature
+int openTapPin = 1;         // open tap manually
+int switchModePin = 1;      // switch operating mode
+int resetButtonPin = 1;     // insert button pin
+//output
+int eletroValve2Pin = 1;    // eletrovalve 2
+int eletroValve1Pin = 1;    // eletrovalve 1
+int lcdPin = 1;             // lcd
+
+// Variables
+int redValue = 0;           // red value lcd 0-255
+int greenValue = 0;         // green value lcd 0-255
+int blueValue = 0;          // blue value lcd 0-255
+
+// Timers
+
+// States
+
+
+// LCD
 rgb_lcd lcd;
 
 void setup() {
-  /*
-  pinMode(pin_number, OUTPUT); // Set a pin to output
-  pinMode(pin_number, INPUT); // Set a pin to input
-  */
+
+  pinMode(tempPin, INPUT);
+  pinMode(moreTempButtonPin, INPUT);
+  pinMode(lessTempButtonPin, INPUT);
+  pinMode(openTapPin, INPUT);
+  pinMode(switchModePin, INPUT);
+  pinMode(resetButtonPin, INPUT);
+
+  pinMode(eletroValve1Pin, OUTPUT);
+  pinMode(eletroValve2Pin, OUTPUT);
+  pinMode(lcdPin, OUTPUT);
+
   Serial.begin(9600);
 
   lcd.begin(16, 2);
-  lcd.setRGB(red_value, green_value, blue_value);
+  lcd.setRGB(redValue, greenValue, blueValue);
 
 
   lcd.newclear(10);
